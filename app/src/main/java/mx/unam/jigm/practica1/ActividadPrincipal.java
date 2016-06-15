49e24cc1444e4c6b529792a738af00afd3edf342
jigm0406 <jigm0406@gmail.com>
2016-06-15 14:06:25 -0500

    ComienzoPractica1

---
+++ app/src/main/java/mx/unam/jigm/practica1/ActividadPrincipal.java
@@ -0,0 +1,13 @@
+package mx.unam.jigm.practica1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActividadPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_principal);
+    }
+}
